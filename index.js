15aa8cf0fdc1b9ded39eac03394827f3d47d73ed
Deepak-afk123 <deepakvadlamudi62@gmail.com>
2024-07-30 16:23:25 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+const esp8266Ip = '192.168.29.79'; // Replace with your ESP8266 IP address
+
 document.getElementById('onButton').addEventListener('click', function() {
-    fetch('http://<YOUR_ESP8266_IP>/led/on')
+    fetch(`${esp8266Ip}/led/on`)
         .then(response => response.text())
         .then(data => console.log(data))
         .catch(error => console.error('Error:', error));
 });
 
 document.getElementById('offButton').addEventListener('click', function() {
-    fetch('http://<YOUR_ESP8266_IP>/led/off')
+    fetch(`${esp8266Ip}/led/off`)
         .then(response => response.text())
         .then(data => console.log(data))
         .catch(error => console.error('Error:', error));
